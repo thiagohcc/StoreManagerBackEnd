2094e73a696b7a9d524b50aac4169f6b3051c6d1
thiagohcc <dzno23@gmail.com>
2023-03-22 19:04:55 -0300

    CHORE: Adiciona ocódigo da rota GET para os todos os produtos.

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const productsControllers = require('../controllers/products.controllers');
 
 const productsRouter = express.Router();
+
+productsRouter.get('/', productsControllers.receiveAllProducts);
 
 module.exports = productsRouter;
