d4f68210ff062a95f05404f598efb6ce6fe0b504
thiagohcc <dzno23@gmail.com>
2023-03-22 18:26:39 -0300

    FEAT: App recebe leitura de JSON + models recebe arquivo connection

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// app.get('/products', (_req, res) => res.status(200).end());
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
