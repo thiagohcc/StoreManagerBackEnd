0b8996a890efd5a05aecee7b329d06af1a14e872
thiagohcc <dzno23@gmail.com>
2023-03-22 18:32:40 -0300

    FEAT: Adiciona ap ap o use da rota de produtos

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const productsRouter = require('./routes/products.routes');
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', productsRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
