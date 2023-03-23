239c2b49af6d5006f676deecef352f4604e50577
thiagohcc <dzno23@gmail.com>
2023-03-23 16:13:02 -0300

    FEAT: Cria a rota para exibir um produto através de um id dinâmico

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,6 +3,8 @@ const productsControllers = require('../controllers/products.controllers');
 
 const productsRouter = express.Router();
 
-productsRouter.get('/', productsControllers.receiveAllProducts);
+productsRouter
+  .get('/', productsControllers.receiveAllProducts)
+  .get('/:id', productsControllers.receiveProductById);
 
 module.exports = productsRouter;
