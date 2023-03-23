1f21cac9aac9a90a86b78403fa70382aa765da3d
thiagohcc <dzno23@gmail.com>
2023-03-23 16:10:14 -0300

    FIX: Corrigindo a configuração do arquivo connect com o banco de dados - Não é nescessário utilizar o .env se for rodar tudo no container - db acessível e retornando dados! VIVA! <3

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-// app.get('/products', (_req, res) => res.status(200).end());
+// app.get('/', (_req, res) => res.status(200).json({ message: 'oi ' }));
+
+// app.get('/', (req, res) => res.status(200).send({ message: 'OK!' }));
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
