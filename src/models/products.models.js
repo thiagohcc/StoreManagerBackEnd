ec25c7b0972860922790cdeb2f2b1c1aeb388c86
thiagohcc <dzno23@gmail.com>
2023-03-23 20:59:52 -0300

    FEAT: Cria camada Models para novo produto

--- src/models/products.models.js
+++ src/models/products.models.js
@@ -13,7 +13,16 @@ const findProductById = async (id) => {
   return data;
 };
 
+const saveNewProduct = async (name) => {
+  const [data] = await connection.execute(
+    'INSERT INTO StoreManager.products (name) VALUES (?)',
+    [name],
+  );
+  return data;
+};
+
 module.exports = {
   findAllProducts,
   findProductById,
+  saveNewProduct,
 };
