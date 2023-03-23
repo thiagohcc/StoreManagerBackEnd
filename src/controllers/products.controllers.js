efbee21ef1d7fa385e5ca2369f527543c10c2cb8
thiagohcc <dzno23@gmail.com>
2023-03-23 16:15:51 -0300

    FEAT: Cria o controller para solicitar e retornar um produto através de um id

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -7,6 +7,13 @@ const receiveAllProducts = async (_req, res) => {
   return res.status(STATUS_OK).json(message);
 };
 
+const receiveProductById = async (req, res) => {
+  const { id } = req.params;
+  const { message } = await productsServices.getProductById(+id);
+  return res.status(STATUS_OK).json(message);
+};
+
 module.exports = {
   receiveAllProducts,
+  receiveProductById,
 };
