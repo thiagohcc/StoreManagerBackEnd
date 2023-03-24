b64595605e4cb605a3a30725bad5263ef22a3bcf
thiagohcc <dzno23@gmail.com>
2023-03-23 22:10:07 -0300

    FEAT: Desenvolve o middleware para validar o nome do produto

---
+++ src/middlewares/product.validate.js
@@ -0,0 +1,20 @@
+const STATUS_BAD_REQUEST = 400;
+const STATUS_UNPROCESSABLE_CONTENT = 422;
+
+const productValidate = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(STATUS_BAD_REQUEST)
+      .json({ message: '"name" is required' });
+  }
+  if (name.length < 5) {
+    return res.status(STATUS_UNPROCESSABLE_CONTENT)
+      .json({ message: '"name" length must be at least 5 characters long' });
+  }
+  console.log('Passou no validador');
+  next();
+};
+
+module.exports = {
+  productValidate,
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const productsControllers = require('../controllers/products.controllers');
+const { productValidate } = require('../middlewares/product.validate');
 
 const productsRouter = express.Router();
 
 productsRouter
   .get('/', productsControllers.receiveAllProducts)
   .get('/:id', productsControllers.receiveProductById)
-  .post('/', productsControllers.createNewProduct);
+  .post('/', productValidate, productsControllers.createNewProduct);
 
 module.exports = productsRouter;
