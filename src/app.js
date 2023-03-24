d095cd2f1a2f5beab8ddf0b2fbb2b964e8be093a
thiagohcc <dzno23@gmail.com>
2023-03-23 22:21:07 -0300

    FEAT: Cria e add no app a rota sales.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const productsRouter = require('./routes/products.routes');
+const salesRouter = require('./routes/sales.routes');
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
