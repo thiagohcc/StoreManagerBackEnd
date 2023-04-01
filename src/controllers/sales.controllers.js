0c7b804fcfee680d567f479131de9520ea50c39c
thiagohcc <dzno23@gmail.com>
2023-04-01 00:17:03 -0300

    FEAT: Cria camada de controller para deletar uma venda

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -2,6 +2,7 @@ const salesServices = require('../services/sales.services');
 
 const STATUS_OK = 200;
 const STATUS_CREATED = 201;
+const STATUS_NO_CONTENT = 204;
 const STATUS_NOT_FOUND = 404;
 
 const receiveAllSales = async (_req, res) => {
@@ -24,8 +25,18 @@ const receiveNewSale = async (req, res) => {
   return res.status(STATUS_CREATED).json(message);
 };
 
+const removeSaleById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await salesServices(id);
+  if (type === 404) {
+    return res.status(STATUS_NOT_FOUND).json(message);
+  }
+  return res.status(STATUS_NO_CONTENT).end();
+};
+
 module.exports = {
   receiveAllSales,
   receiveSaleById,
   receiveNewSale,
+  removeSaleById,
 };
